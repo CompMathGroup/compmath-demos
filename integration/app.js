cd7a1f642d8fca92ad8c97cdedec427be9526477
Ivan Tsybulin <tsybulinhome@gmail.com>
2014-09-22 14:18:02 +0400

    minor

--- integration/app.js
+++ integration/app.js
@@ -80,7 +80,6 @@ function Model() {
             for (i = 0; i < ord; i++) {
                 sum += c[i] / (i - al + 1) * (Math.pow(this.b, i - al + 1) - Math.pow(this.a, i - al + 1))
             }
-            console.log(sum);
 
             this.phi = [math.parse(v), math.parse(z), al];
             this.I1 = sum;
@@ -203,7 +202,7 @@ function View(model, controls) {
         $("#phi3").val(this.model.phi[2]);
         $("#phiint").val(this.model.I1);
 
-        var prefix = "/integration/";
+        var prefix = "";
 
         $("#quad").attr("src", prefix + this.model.quad.tex);
         $("#quaderr").attr("src", prefix + this.model.quad.err);
